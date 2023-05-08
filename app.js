223893fe49a6330f49ae970a37193cbe0193c5c8
jorgen eikemo <jorgen.eikemo@gmail.com>
2023-05-08 22:11:19 +0200

    another try

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
   proxy: true, 
     name: 'movieDBcookies', 
     cookie: {
-      secure: true,
+      secure: false,
       httpOnly: false,
       maxAge: 1000 * 60 * 60 * 48,
       sameSite: 'none'
